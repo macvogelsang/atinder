7b53d553f7f297021a73acb753b6cc6770b4f0e9
cholical <ronding@live.unc.edu>
2016-10-31 02:28:40 -0400

    Another database quickie

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
 create database atinder;
 use atinder;
 create table events ( eventId varchar(255) PRIMARY KEY, dateStart varchar(255),  dateEnd varchar(255),  checkStart varchar(255),  checkEnd varchar(255),  adminId varchar(255),  name varchar(255),  description long,  number varchar(255), timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP )
-create table check_ins (  id int not null auto_increment primary key,  number varchar(255),  eventId varchar(255),  content long )
+create table check_ins (  id int not null auto_increment primary key,  number varchar(255),  eventId varchar(255),  content long, timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);
